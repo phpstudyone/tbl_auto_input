31ccf06f608091e05889f60f554451ab1cb5cf76
Jason Zhang <jaszhang@xogrp.com>
2018-01-09 19:36:58 +0800

    create album page finished

--- js/common.js
+++ js/common.js
@@ -2,6 +2,19 @@
 // chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
 // });
 
+commom = {
+    'getAfterNdate' : (n)=>{
+        let date = new Date(); 
+        let year,month,day ;  
+        date.setDate(date.getDate()+n);  
+        year = date.getFullYear();  
+        month = date.getMonth()+1;  
+        day = date.getDate() ;  
+        s = ( month < 10 ? ( '0' + month ) : month ) + '/' + ( day < 10 ? ( '0' + day ) : day) + '/' + year ;  
+        return s ; 
+    }
+};
+
 url = new URL(window.location.href);
 superPassword = '';
 adminInfo = {
@@ -44,6 +57,38 @@ pageFunction = {
     'administrator':()=>{
         document.getElementById('UserEmail').value = adminInfo.username;
         document.getElementById('UserLoginPassword').value = adminInfo.password;
+    },
+    '/albums/create_album':()=>{
+        document.getElementsByClassName('albumTypeSelect')[0].click();
+        let i = 1 , sleep = 1000;
+        setTimeout(function(){
+           document.getElementsByClassName('categoryBlock')[0].click();
+        },sleep*i++);
+
+        setTimeout(function(){
+           document.getElementById('real').click();
+        },sleep*i++);
+
+        setTimeout(function(){
+            document.getElementById('WeddingBrideFirstName').value = 'Jason';
+            document.getElementById('WeddingBrideLastName').value = 'Zhang';
+            document.getElementById('WeddingGroomFirstName').value = 'Jason';
+            document.getElementById('WeddingGroomLastName').value = 'Zhang';
+            document.getElementById('wedding_date').value = commom['getAfterNdate'](15);
+            document.getElementById('locationCity').value = 'City';
+            document.getElementById('WeddingTitle').value = 'test add album title';
+            document.getElementById('nextSuccessBtn').click();
+        },sleep*i++);
+
+        setTimeout(() => {
+            document.getElementById('venue_private_residence').click();
+            document.getElementById('tagsShowingBtn').click();
+            
+        }, sleep*i++);
+
+        setTimeout(() => {
+            document.getElementsByClassName('btn-blue albSaveFinal tagSaveBtn new_dom test-nextdone')[0].click();
+        }, sleep*i++);
     }
 }
 console.log(url);
