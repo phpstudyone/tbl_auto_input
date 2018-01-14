d37b1ff18d2a619f61ac5fa1980cfcc2b10aa519
Jasong Zhang <845830229@qq.com>
2018-01-14 15:23:28 +0800

    点击插件头标填充页头登陆表单  改为  唤出登陆框就填充表单

---
+++ js/common_end.js
@@ -0,0 +1,25 @@
+/**
+ * 页面加载完毕之后注入的js文件
+ */
+
+let superPassword = '';
+let adminInfo = {
+    username:'tblgenadmin@twobrightlights.com',
+    password:'dkI_ef3Hd'
+};
+
+if(url.host === 'qa-www.twobrightlights.com'){
+    superPassword = 'aT2thg_Git8';
+}else if(url.host === 'local.tbl.com'){
+    superPassword = '123456789';
+}
+
+//当顶部登录窗口处于打开状态,点击扩展图标自动填充
+let topLoginButton = document.getElementsByClassName('dropdown-toggle blueHighlight');
+if(topLoginButton.length === 1){
+    topLoginButton[0].onclick=function(){
+        let frmLoginTop = document.forms.frmLoginTop;
+        frmLoginTop.UserEmail.value = 'gzp2@twobrightlights.com';
+        frmLoginTop.UserPassword.value = superPassword;
+    };
+}
